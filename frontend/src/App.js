1ea1a6901947c0d1a15b940963e551ce21a4f1fe
Danny Mendler <dmendler@Dannys-Work-Mac.local>
2026-08-05 14:35:42 -0500

    added 4th view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './utils/ThemeContext';
-import OrdersView   from './pages/OrdersView';
-import ProductsView from './pages/ProductsView';
-import CustomersView from './pages/CustomersView';
+import OrdersView          from './pages/OrdersView';
+import ProductsView        from './pages/ProductsView';
+import CustomersView       from './pages/CustomersView';
+import CustomerHistoryView from './pages/CustomerHistoryView';
 import './App.css';
 
 export default function App() {
@@ -11,11 +12,12 @@ export default function App() {
     <ThemeProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/"          element={<Navigate to="/orders" replace />} />
-          <Route path="/orders"    element={<OrdersView />} />
-          <Route path="/products"  element={<ProductsView />} />
-          <Route path="/customers" element={<CustomersView />} />
-          <Route path="*"          element={<Navigate to="/orders" replace />} />
+          <Route path="/"                      element={<Navigate to="/orders" replace />} />
+          <Route path="/orders"                element={<OrdersView />} />
+          <Route path="/products"              element={<ProductsView />} />
+          <Route path="/customers"             element={<CustomersView />} />
+          <Route path="/customers/:customerId" element={<CustomerHistoryView />} />
+          <Route path="*"                      element={<Navigate to="/orders" replace />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
